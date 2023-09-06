a67a3858d26731476736b395ac823a7c92eb1fb5
Tibi <tyby_tiberiu@yahoo.com>
2023-09-06 23:33:09 +0300

    Added email and password validations for user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,36 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        
-         has_many :posts
-         
-         has_many :comments
+         :recoverable, :rememberable
 
-         has_one_attached :avatar
+  has_many :posts
 
-         
+  has_many :comments
 
-         before_create :randomize_id
-         private
-          def randomize_id
-            begin
-              self.id = SecureRandom.random_number(1_000_000_000)
-            end while User.where(id: self.id).exists?
-          end
+  has_one_attached :avatar
+
+  before_create :randomize_id
+
+  private
+
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number(1_000_000_000)
+    end while User.where(id:).exists?
+  end
+
+  # Validations
+  # Email validations----------------
+  validates :email, presence: true, length: { maximum: 254 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # # Password validations-------------
+  PASSWORD_FORMAT = /\A
+    (?=.*[A-Z]) # Must contain an uppercase character
+    (?=.*[a-z]) # Must contain a lowercase character
+    (?=.*\d) # Must contain a digit
+    # (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x
+
+  validates :password, presence: true, length: { minimum: 8, maximum: 128 },
+                       format: { with: PASSWORD_FORMAT, message: 'must include: 1 uppercase, 1 lowercase and 1 digit'}
 end
